42ae58c4430e95df106c0ff02ff006ddd2472bcd
RoughGhost <Stephendlouis6@gmail.com>
2022-01-17 23:04:05 +0100

    Added More Functionality

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
+import { submitComment } from "../services";
 
-const CommentsForm = (slug) => {
+const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
@@ -8,6 +9,11 @@ const CommentsForm = (slug) => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  }, []);
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -29,17 +35,25 @@ const CommentsForm = (slug) => {
       slug,
     };
     if (storeData) {
-      localStorage.setItem("name", name);
-      localStorage.setItem("email", email);
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
     } else {
-      localStorage.removeItem("name", name);
-      localStorage.removeItem("email", email);
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
     }
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Leave a Reply
+      </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
           ref={commentEl}
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -2,14 +2,42 @@
  *Any File Inside the folder pages/api is mapped to /api/* and *
  * will be treated as an API endpoint instead of a page. ****
  */
-import { GraphQLClient, gql } from "graphql";
+import { GraphQLClient, gql } from "graphql-request";
 
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export default function comments(req, res) {
+export default async function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+      authorization: `Bearer ${graphcmsToken}`,
     },
   });
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `;
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
+
+  return res.status(200).send(result);
 }
